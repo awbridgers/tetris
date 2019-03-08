278f3eb5efbfa66716d7c82f3c7cb9ee6ae8335f
awbridgers <awbridgers@users.noreply.github.com>
2019-03-08 17:00:05 -0500

    adds different colors to the shapes

--- src/containers/canvas.js
+++ src/containers/canvas.js
@@ -24,23 +24,62 @@ class Canvas extends Component{
         ctx.rect(x*36,y*36,35,35);
         //if it is a 0, nothing in square, color grey
         if(square === 0){
-          ctx.fillStyle = 'grey';
-          ctx.fill();
+          ctx.fillStyle = '#2E2E2E';
         }
-        //if its a tetris piece, color it in
-        else if (square === 1){
-          ctx.fillStyle = 'red';
-          ctx.fill();
+        if(square === 1){
+          ctx.fillStyle = '#FFFF00';
         }
+        else if (square === 2){
+          ctx.fillStyle = '#00FFFF';
+        }
+        else if (square === 3){
+          ctx.fillStyle = '#00FF00';
+        }
+        else if (square === 4){
+          ctx.fillStyle = '#FF0000';
+        }
+        else if (square === 5){
+          ctx.fillStyle = '#8000FF';
+        }
+        else if (square === 6){
+          ctx.fillStyle = '#FF8000';
+        }
+        else if (square === 7){
+          ctx.fillStyle = '#0000FF';
+        }
+
+        ctx.fill();
       })
     })
     // now we need to color in the current tetris piece that is coming down the board
     tetronimo.shape.forEach((row,i) =>{
-      row.forEach((col,j)=>{
+      row.forEach((square,j)=>{
         ctx.beginPath();
         ctx.rect((j + tetronimo.topLeft.col)*36, (i + tetronimo.topLeft.row) * 36, 35,35)
         ctx.fillStyle = 'red';
-        if(row !==0 && col !==0){
+        if(row !==0 && square !==0){
+          //if its a tetris piece, color it in
+          if(square === 1){
+            ctx.fillStyle = '#FFFF00';
+          }
+          else if (square === 2){
+            ctx.fillStyle = '#00FFFF';
+          }
+          else if (square === 3){
+            ctx.fillStyle = '#00FF00';
+          }
+          else if (square === 4){
+            ctx.fillStyle = '#FF0000';
+          }
+          else if (square === 5){
+            ctx.fillStyle = '#8000FF';
+          }
+          else if (square === 6){
+            ctx.fillStyle = '#FF8000';
+          }
+          else if (square === 7){
+            ctx.fillStyle = '#0000FF';
+          }
           ctx.fill();
         }
         ctx.closePath();
--- src/reducers/currentTetro.js
+++ src/reducers/currentTetro.js
@@ -1,6 +1,6 @@
 import tetroArray from '../shapes.js'
 
-export const currentTetro = (state = tetroArray[1], action)=>{
+export const currentTetro = (state = tetroArray[5], action)=>{
   switch(action.type){
     case 'UPDATE_TETRO':
       return action.tetro;
