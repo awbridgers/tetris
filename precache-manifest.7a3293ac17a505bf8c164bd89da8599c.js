0a507b6a8747d150eeef5a2d3f96cc825f9162a0
awbridgers <awbridgers@users.noreply.github.com>
2019-03-20 13:35:06 -0400

    Updates

--- precache-manifest.7a3293ac17a505bf8c164bd89da8599c.js
+++ precache-manifest.7a3293ac17a505bf8c164bd89da8599c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/tetris/static/js/runtime~main.18d269b3.js"
   },
   {
-    "revision": "bd48b8f76384b9605766",
-    "url": "/tetris/static/js/main.bd48b8f7.chunk.js"
+    "revision": "25a6464ab1612860daa7",
+    "url": "/tetris/static/js/main.25a6464a.chunk.js"
   },
   {
     "revision": "0d851737320b98d2ec16",
     "url": "/tetris/static/js/2.0d851737.chunk.js"
   },
   {
-    "revision": "bd48b8f76384b9605766",
-    "url": "/tetris/static/css/main.4b06f27f.chunk.css"
+    "revision": "25a6464ab1612860daa7",
+    "url": "/tetris/static/css/main.ec8c450b.chunk.css"
   },
   {
-    "revision": "fc799ff5e4f6484bf2d0c93dfd2707b4",
+    "revision": "a8e73d698ef7ec2e8f378c8c6d03c1be",
     "url": "/tetris/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tetris/precache-manifest.f931b234ebae70f4daedb40a61216bc1.js"
+  "/tetris/precache-manifest.7a3293ac17a505bf8c164bd89da8599c.js"
 );
 
 workbox.clientsClaim();
