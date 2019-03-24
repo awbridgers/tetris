09b7ccb5875badfa2068eb5898e50007d58058f9
awbridgers <awbridgers@users.noreply.github.com>
2019-03-24 16:35:58 -0400

    Updates

--- precache-manifest.3d18ab678f6ca6b6ebe1d0cbd3efc4b5.js
+++ precache-manifest.3d18ab678f6ca6b6ebe1d0cbd3efc4b5.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/tetris/static/js/runtime~main.18d269b3.js"
   },
   {
-    "revision": "135e060f830b1f5b1e9b",
-    "url": "/tetris/static/js/main.135e060f.chunk.js"
+    "revision": "5103101ee2a529c6556b",
+    "url": "/tetris/static/js/main.5103101e.chunk.js"
   },
   {
     "revision": "d9e78faf2db759e6a22d",
     "url": "/tetris/static/js/2.d9e78faf.chunk.js"
   },
   {
-    "revision": "135e060f830b1f5b1e9b",
+    "revision": "5103101ee2a529c6556b",
     "url": "/tetris/static/css/main.834f6085.chunk.css"
   },
   {
-    "revision": "c23448893423eaf60f3d4a60e0bb0cd4",
+    "revision": "1a198ad033182491892a89c574414c0b",
     "url": "/tetris/index.html"
   }
 ];
